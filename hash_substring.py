e50c65a5431fe9a650e7096c8f6e582d9b08b686
Rinalds Ulmanis 221RDB152 <118060249+Rinalds13@users.noreply.github.com>
2023-04-17 00:30:47 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,6 +1,7 @@
-# python3
+#python 3
 
 def read_input():
+
     ievade = input()
     if ievade.startswith('F'):
         with open('tests/06', 'r') as f:
@@ -8,11 +9,11 @@ def read_input():
     else:
         modelis, teksts = input().strip(), input().strip()
     return teksts, modelis
-    return (input().rstrip(), input().rstrip())
+
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
+
 
 def get_occurrences(pattern, text):
     indeks = []
@@ -22,15 +23,13 @@ def get_occurrences(pattern, text):
     for i in range(pg - 1):
         q = (q * ag)
     hash_ver = nev = i = j = h = 0
-    # and return an iterable variable
-    return [0]
-    
+
     for i in range(pg):
-    hash_ver = (hash_ver * ag + ord(pattern[i])) % q
-    nev = (nev * ag + ord(text[i])) % q
-    if i < pg - 1:
-        h = (ag * h + 1) % q
-        
+        hash_ver = (hash_ver * ag + ord(pattern[i])) % q
+        nev = (nev * ag + ord(text[i])) % q
+        if i < pg - 1:
+            h = (ag * h + 1) % q
+
     for i in range(tg - pg + 1):
         if hash_ver == nev and pattern == text[i:i+pg]:
             indeks.append(str(i))
@@ -38,7 +37,5 @@ def get_occurrences(pattern, text):
             nev = (ag * (nev - ord(text[i]) * h) + ord(text[i + p_len])) % q
     return indeks
 
-# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
