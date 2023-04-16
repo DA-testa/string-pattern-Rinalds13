61053b2e9755272a6ad0bb54dde7e062c7874f1e
Rinalds Ulmanis 221RDB152 <118060249+Rinalds13@users.noreply.github.com>
2023-04-17 00:59:01 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,38 +1,58 @@
-def get_input_type():
-    while True:
-        input_type = input().strip().upper()
-        if input_type in ('K', 'F'):
-            return input_type
-        else:
-            print("error")
+def read_input():
+    input_type = input().strip().upper()
+
+    if input_type == "I":
+        pattern = input().strip()
+        text = input().strip()
+        return (input_type, pattern, text)
+    elif input_type == "F":
+        with open("tests/06") as f:
+            pattern = f.readline().strip()
+            text = f.readline().strip()
+        return (input_type, pattern, text)
+    else:
+        exit()
 
 
-def read_from_keyboard():
-    pattern = input().strip()
-    text = input().strip()
-    return pattern, text
-
-
-def read_from_file():
-    filename = input()
-    with open(filename) as f:
-        pattern = f.readline().strip()
-        text = f.readline().strip()
-    return pattern, text
-
-
-def find_occurrences(pattern, text):
+def rabin_karp(pattern, text):
     p = len(pattern)
     t = len(text)
-    pattern_hash = sum(ord(pattern[i]) * pow(10, p - i - 1) for i in range(p))
-    text_hash = sum(ord(text[i]) * pow(10, p - i - 1) for i in range(p))
-    occurrences = []
+    if p > t:
+        return []
+
+    prime = 1000000007
+    multiplier = 263
+    pattern_hash = 0
+    text_hash = 0
+    result = []
+    h = 1
+
+    for i in range(p - 1):
+        h = (h * multiplier) % prime
+
+    for i in range(p):
+        pattern_hash = (pattern_hash * multiplier + ord(pattern[i])) % prime
+        text_hash = (text_hash * multiplier + ord(text[i])) % prime
+
     for i in range(t - p + 1):
-        if pattern_hash == text_hash and pattern == text[i:i+p]:
-            occurrences.append(i)
+        if pattern_hash == text_hash:
+            if pattern == text[i:i + p]:
+                result.append(i)
         if i < t - p:
-            text_hash = (text_hash - ord(text[i]) * pow(10, p - 1)) * 10 + ord(text[i+p])
-    return occurrences
+            text_hash = (multiplier * (text_hash - ord(text[i]) * h) + ord(text[i + p])) % prime
+            if text_hash < 0:
+                text_hash += prime
+    return result
+
+
+def print_occurrences(occurrences):
+    print(" ".join(str(x) for x in occurrences))
+
+
+if __name__ == '__main__':
+    input_type, pattern, text = read_input()
+    occurrences = rabin_karp(pattern, text)
+    print_occurrences(occurrences)
 
 
 def print_occurrences(occurrences):
